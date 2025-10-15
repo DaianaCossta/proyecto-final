100c9c6f7d24630257cacc78a211185f9e0fb139
DaianaCossta <daiianacosta14@hotmail.com>
2025-10-15 20:06:38 -0300

    agregado array de juegosy renderizado multiple con map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import './App.css';
 
 function App() {
-  // Datos de prueba de un juego
-  const juego = {
-    titulo: "The Last of Us",
-    genero: "Aventura",
+ //array de varios juegos//
+  const juegos = [
+    {
+      id: 1,
+      titulo: "The Last of Us",
+      genero: "Aventura",
+      completado: true,
+      puntuacion: 5
+    },
+    {
+      id: 2,
+      titulo: "Hollow Knight",
+      genero: "Metroidvania",
+      completado: true,
+      puntuacion: 5
+    },
+    {
+      id: 3,
+      titulo: "Elden Ring",
+      genero: "RPG",
+      completado: false,
+      puntuacion: 4
+    },
+    {
+    id: 4,
+    titulo: "Stardew Valley",
+    genero: "Simulación",
     completado: true,
     puntuacion: 5
-  };
+  }
+
+  ];
 
   return (
     <div className="App">
       <h1>🎮 GameTracker</h1>
       
-      <div className="tarjeta-juego">
-        <h2>{juego.titulo}</h2>
-        <p>Género: {juego.genero}</p>
-        <p>⭐ Puntuación: {juego.puntuacion}/5</p>
-        <p>{juego.completado ? "✅ Completado" : "⏳ En progreso"}</p>
+      <div className="biblioteca">
+        {juegos.map((juego) => ( //recorre el array y crea una tarjeta para cada juego//
+           //key para que react identifique que elementos cambiaron, se agregaron o eliminaron//
+          <div key={juego.id} className="tarjeta-juego"> 
+            <h2>{juego.titulo}</h2>
+            <p>Género: {juego.genero}</p>
+            <p>⭐ Puntuación: {juego.puntuacion}/5</p>
+            <p>{juego.completado ? "✅ Completado" : "⏳ En progreso"}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
